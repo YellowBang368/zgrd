738070ee95b4f2b3746827f79fbd35784444e001
TopClone <yellowbang368@gmail.com>
2018-08-24 19:20:15 +0300

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,6 @@ module ApplicationHelper
   def put_location(user)
     geoip = GeoIP.new(Rails.root.join('lib/GeoIP.dat'))
     geoip.country(request.remote_ip).country_name
-    return request.remote_ip
   end
 
 end
