bc0865b580b286e99564f48e6399611ab4c34d16
TopClone <yellowbang368@gmail.com>
2018-08-24 19:27:01 +0300

    Fixed nil case

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,11 @@ module ApplicationHelper
 
   def put_location(user)
     geoip = GeoIP.new(Rails.root.join('lib/GeoIP.dat'))
-    geoip.country(request.remote_ip).country_name
+    if request.remote_ip = "127.0.0.1"
+      "87.70.63.132"
+    else
+      geoip.country(request.remote_ip).country_name
+    end
   end
 
 end
